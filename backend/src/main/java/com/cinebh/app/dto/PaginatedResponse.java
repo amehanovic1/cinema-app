0093021d30d2b8915ff658c5691d07c6e9978dfc
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-10 08:42:29 +0100

    backend: Add pagination utility and PagintedResponse

---
+++ backend/src/main/java/com/cinebh/app/dto/PaginatedResponse.java
@@ -0,0 +1,22 @@
+package com.cinebh.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaginatedResponse<T> {
+    private List<T> data;
+    private int currentPage;
+    private int totalPages;
+    private long totalItems;
+    private int pageSize;
+    private boolean hasNext;
+    private boolean hasPrevious;
+}
