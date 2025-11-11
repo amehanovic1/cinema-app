482dbd5bec985fe89ad4395fd74c475f689e5dc6
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-11 09:26:40 +0100

    backend: Remove SeatType

--- backend/src/main/java/com/cinebh/app/repository/SeatTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cinebh.app.repository;
-
-import com.cinebh.app.entity.SeatType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SeatTypeRepository extends JpaRepository<SeatType, UUID> {
-}
