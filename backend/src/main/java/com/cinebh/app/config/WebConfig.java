7da0bc3335a40ffd2b5679ba5acdcaf56c1099e1
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-13 21:45:20 +0100

    backend: Configure CORS using .env and define allowed headers

--- backend/src/main/java/com/cinebh/app/config/WebConfig.java
+++ backend/src/main/java/com/cinebh/app/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.cinebh.app.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
+                .allowedHeaders(
+                        "Content-Type",
+                        "Authorization",
+                        "Accept",
+                        "Origin",
+                        "X-Requested-With"
+                )
                 .allowCredentials(true);
     }
 }
