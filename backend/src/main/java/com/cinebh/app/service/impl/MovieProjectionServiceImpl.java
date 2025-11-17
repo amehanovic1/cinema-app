a4defbe9abd95cc72908ba72a47502b3c803910b
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-18 00:05:22 +0100

    backend: Implement service logic for Genre, City and MovieProjection

--- backend/src/main/java/com/cinebh/app/service/impl/MovieProjectionServiceImpl.java
+++ backend/src/main/java/com/cinebh/app/service/impl/MovieProjectionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cinebh.app.service.impl;
 
 import com.cinebh.app.dto.MovieProjectionDto;
+<<<<<<< HEAD
 import com.cinebh.app.entity.MovieProjection;
 import com.cinebh.app.mapper.MovieProjectionMapper;
 import com.cinebh.app.repository.MovieProjectionRepository;
@@ -8,6 +9,12 @@ import com.cinebh.app.service.MovieProjectionService;
 import com.cinebh.app.specification.MovieProjectionSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
+=======
+import com.cinebh.app.mapper.MovieProjectionMapper;
+import com.cinebh.app.repository.MovieProjectionRepository;
+import com.cinebh.app.service.MovieProjectionService;
+import lombok.RequiredArgsConstructor;
+>>>>>>> 20bc46d (backend: Implement service logic for Genre, City and MovieProjection)
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
