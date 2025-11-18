5189a12b78790466a90db8f764c5ad32ae3f8b14
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-18 15:09:50 +0100

    frontend: Move Header, Footer and ScrollToTop to layouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import Footer from './layouts/Footer/Footer';
+import Header from './layouts/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import AboutUs from './pages/AboutUs/AboutUs';
 import Tickets from './pages/Tickets/Tickets';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import ScrollToTop from './layouts/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
