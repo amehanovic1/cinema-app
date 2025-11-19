a94adba2c815f9dd2cfcfabd3d4c748e7298a58a
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-19 15:10:38 +0100

    frontend: Refactor DatePicker component

--- frontend/src/components/DatePicker/DatePicker.jsx
+++ frontend/src/components/DatePicker/DatePicker.jsx
@@ -2,51 +2,63 @@ import { useEffect, useState } from "react";
 
 const DatePicker = ({ numberOfDays = 10, onChange }) => {
 
-    const [currentDate, setCurrentDate] = useState(new Date())
+    const [selectedDate, setSelectedDate] = useState(new Date())
     const [dates, setDates] = useState([])
+    const [todayDate] = useState(new Date())
 
     useEffect(() => {
-        generateDates();
+        createDateRange();
     }, [])
 
-    const generateDates = () => {
-        const startDate = new Date()
-        const array = []
-        for (let i = 0; i < numberOfDays; i++) {
-            const newDate = new Date(startDate)
-            newDate.setDate(newDate.getDate() + i)
-            array.push(newDate)
-        }
-        setDates(array)
-    } 
-    
     const handleChange = (date) => {
-        setCurrentDate(date);
+        setSelectedDate(date);
         if (onChange) onChange(date.toLocaleDateString('sv-SE'));
+    }
+
+    const createDateRange = () => {
+        const today = new Date()
+        const dateList = []
+        for (let i = 0; i < numberOfDays; i++) {
+            const newDate = new Date(today)
+            newDate.setDate(newDate.getDate() + i)
+            dateList.push(newDate)
+        }
+        setDates(dateList)
+    }
+
+    const isSameDate = (d1, d2) => {
+       return (
+        d1.getDate() === d2.getDate() &&
+        d1.getMonth() === d2.getMonth() &&
+        d1.getFullYear() === d2.getFullYear()
+       );
     }
 
     return (
         <div className="flex flex-row flex-wrap items-center justify-between gap-2 sm:gap-4 md:gap-6 lg:gap-8 justify-start">
             {dates.map((date, index) =>
-               
-               <button
+
+                <button
                     key={index}
                     className={`
                         flex flex-col items-center justify-center
                         px-3 py-1 sm:px-4 sm:py-2 md:px-5 md:py-3
                         rounded-xl shadow-card
                         text-xs sm:text-sm md:text-base
-                        ${date.getDate() === currentDate.getDate() 
+                        ${date.getDate() === selectedDate.getDate()
                             ? "bg-dark-red text-white font-bold" : "bg-neutral-0"}`}
-                            onClick={() => handleChange(date)}>
-                   
-                    <div className="flex gap-2">
+                    onClick={() => handleChange(date)}>
+
+                    <div className="flex gap-2 font-bold">
                         <h1>{date.toLocaleDateString('en-US', { month: 'short' })} </h1>
                         <h1>{date.toLocaleDateString('en-US', { day: 'numeric' })} </h1>
                     </div>
 
                     <div>
-                        <h1>{date.toLocaleDateString('en-US', { weekday: 'short' })}</h1>
+                        <h1>{isSameDate(todayDate, date)
+                            ? "Today"
+                            : date.toLocaleDateString('en-US', { weekday: 'short' })}
+                        </h1>
                     </div>
                 </button>)}
         </div>
