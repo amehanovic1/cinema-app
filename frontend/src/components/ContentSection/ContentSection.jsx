2194df821c8df102d8f0be65fc8d103494de5f8a
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-20 11:12:35 +0100

    frontend: Refactor ContentSection prop

--- frontend/src/components/ContentSection/ContentSection.jsx
+++ frontend/src/components/ContentSection/ContentSection.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import Pagination from "../Pagination/Pagination";
 
-const ContentSection = ({ title, linkTo = "", items, getAll, renderItem }) => {
+const ContentSection = ({ title, linkTo = null, items, getAll, renderItem }) => {
     return (
         <div className="bg-neutral-25 px-4 sm:px-6 md:px-8 lg:px-12 py-2 sm:py-3 md:py-4 ly:py-5">
 
