f5e57c7424f28030ef128237a8a19668bbaf3683
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-22 15:36:03 +0100

    backend: Remove unused imports

--- backend/src/main/java/com/cinebh/app/service/MovieProjectionService.java
+++ backend/src/main/java/com/cinebh/app/service/MovieProjectionService.java
@@ -3,7 +3,6 @@ package com.cinebh.app.service;
 import com.cinebh.app.dto.MovieProjectionDto;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.UUID;
 
--- backend/src/main/java/com/cinebh/app/service/impl/MovieProjectionServiceImpl.java
+++ backend/src/main/java/com/cinebh/app/service/impl/MovieProjectionServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.UUID;
 
