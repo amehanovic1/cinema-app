07584d3f88cff727dda34b3c1b2ff3144ca3d0a5
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-24 10:08:59 +0100

    backend: Sort genres in MovieDto after mapping

--- backend/src/main/java/com/cinebh/app/mapper/MovieMapper.java
+++ backend/src/main/java/com/cinebh/app/mapper/MovieMapper.java
@@ -1,11 +1,25 @@
 package com.cinebh.app.mapper;
 
+import com.cinebh.app.dto.GenreDto;
 import com.cinebh.app.dto.MovieDto;
 import com.cinebh.app.entity.Movie;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import java.util.Comparator;
 
 @Mapper(componentModel = "spring", uses = {MovieImageMapper.class, GenreMapper.class})
 public interface MovieMapper {
 
     MovieDto toDto(Movie movie);
+
+    @AfterMapping
+    default void sortGenres(Movie movie, @MappingTarget MovieDto movieDto) {
+        if (movieDto.getGenres() != null ) {
+            movieDto.setGenres(movieDto.getGenres().stream()
+                    .sorted(Comparator.comparing(GenreDto::getName))
+                    .toList());
+        }
+    }
 }
