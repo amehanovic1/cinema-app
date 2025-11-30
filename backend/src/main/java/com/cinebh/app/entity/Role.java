20cc2322f80e1931f2449dd0fed22004f7cbb2f3
amehanovic1 <adna.mehanovic1@gmail.com>
2025-11-30 17:05:31 +0100

    backend: Add User and Role entities

---
+++ backend/src/main/java/com/cinebh/app/entity/Role.java
@@ -0,0 +1,26 @@
+package com.cinebh.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+}
