28bb0786b51f61f98b8e67e2dab98e128eff01e7
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-03 10:15:41 +0100

    backend: Add findByName to RoleRepository

--- backend/src/main/java/com/cinebh/app/repository/RoleRepository.java
+++ backend/src/main/java/com/cinebh/app/repository/RoleRepository.java
@@ -3,7 +3,10 @@ package com.cinebh.app.repository;
 import com.cinebh.app.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+    Optional<Role> findByName(String name);
 }
