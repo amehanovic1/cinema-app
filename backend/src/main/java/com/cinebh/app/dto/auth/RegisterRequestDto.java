1274c4804f5327b6227ca962a07db0ee7a791bf1
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-04 13:57:53 +0100

    backend: Add password complexity and length validation

--- backend/src/main/java/com/cinebh/app/dto/auth/RegisterRequestDto.java
+++ backend/src/main/java/com/cinebh/app/dto/auth/RegisterRequestDto.java
@@ -1,8 +1,6 @@
 package com.cinebh.app.dto.auth;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -12,11 +10,15 @@ import lombok.*;
 @AllArgsConstructor
 public class RegisterRequestDto {
 
-    @NotBlank(message = "Email is required")
+    @NotBlank(message = "Email must not be empty")
     @Email(message = "Invalid email format")
     private String email;
 
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
+    @NotBlank(message = "Password must not be empty")
+    @Size(min = 8, max = 48, message = "Password must be between 8 and 48 characters")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).*$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character (@#$%^&+=)"
+    )
     private String password;
 }
