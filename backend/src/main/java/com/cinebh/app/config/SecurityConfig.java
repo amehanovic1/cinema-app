123a5b5fd8f690345737f00d183f8c4dee23812f
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-04 23:02:17 +0100

    backend: Add dev profile to SwaggerConfig

--- backend/src/main/java/com/cinebh/app/config/SecurityConfig.java
+++ backend/src/main/java/com/cinebh/app/config/SecurityConfig.java
@@ -1,35 +1,46 @@
 package com.cinebh.app.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    private final Environment environment;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/api/**",
+                .authorizeHttpRequests(auth -> {
+                    if (isDevProfile()) {
+                        auth.requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
                                 "/v3/api-docs"
-                        ).permitAll()
-                        .anyRequest().authenticated()
-                )
+                        ).permitAll();
+                    }
+                     auth.requestMatchers("/api/**").permitAll()
+                             .anyRequest().authenticated();
+                })
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
 
         return http.build();
+    }
+
+    private boolean isDevProfile() {
+        return environment.matchesProfiles("dev");
     }
 }
