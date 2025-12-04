58ba78315de27ba69ff5ec31fc0e0e40a42be66c
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-04 14:36:38 +0100

    backend: Remove unused RequiredArgsConstructor

--- backend/src/main/java/com/cinebh/app/config/SecurityConfig.java
+++ backend/src/main/java/com/cinebh/app/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/**",
+                                "/api/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
