d4049f7e771bfc4f55b4172fe60ac598ebf1e0cf
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-07 12:16:56 +0100

    backend: Implement sending verification codes and verifying users

--- backend/src/main/java/com/cinebh/app/service/AuthenticationService.java
+++ backend/src/main/java/com/cinebh/app/service/AuthenticationService.java
@@ -1,9 +1,15 @@
 package com.cinebh.app.service;
 
 import com.cinebh.app.dto.auth.AuthResponseDto;
+import com.cinebh.app.dto.auth.EmailRequestDto;
+import com.cinebh.app.dto.auth.VerifyRequestDto;
 import com.cinebh.app.dto.auth.RegisterRequestDto;
 
 public interface AuthenticationService {
 
     AuthResponseDto register(RegisterRequestDto request);
+
+    AuthResponseDto verify(VerifyRequestDto verifyRequestDto);
+
+    AuthResponseDto resendCode(EmailRequestDto request);
 }
--- backend/src/main/java/com/cinebh/app/service/impl/AuthenticationServiceImpl.java
+++ backend/src/main/java/com/cinebh/app/service/impl/AuthenticationServiceImpl.java
@@ -1,18 +1,25 @@
 package com.cinebh.app.service.impl;
 
 import com.cinebh.app.dto.auth.AuthResponseDto;
+import com.cinebh.app.dto.auth.EmailRequestDto;
+import com.cinebh.app.dto.auth.VerifyRequestDto;
 import com.cinebh.app.dto.auth.RegisterRequestDto;
+import com.cinebh.app.entity.EmailVerificationCodes;
 import com.cinebh.app.entity.Role;
 import com.cinebh.app.entity.User;
+import com.cinebh.app.repository.EmailVerificationCodesRepository;
 import com.cinebh.app.repository.RoleRepository;
 import com.cinebh.app.repository.UserRepository;
 import com.cinebh.app.service.AuthenticationService;
+import com.cinebh.app.service.EmailService;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,6 +28,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
+    private final EmailVerificationCodesRepository verificationCodesRepository;
 
     @Override
     public AuthResponseDto register(RegisterRequestDto request) {
@@ -29,8 +38,18 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         Optional<User> optionalUser = userRepository.findByEmail(email);
 
         if(optionalUser.isPresent()) {
+            User user = optionalUser.get();
+
+            if (user.getIsVerified()) {
+                return AuthResponseDto.builder()
+                        .isVerified(true)
+                        .message("This email is already registered. Please log in to continue.")
+                        .success(false)
+                        .build();
+            }
             return AuthResponseDto.builder()
-                    .message("Account with this email already exists!")
+                    .isVerified(false)
+                    .message("Account exists but is not verified. Please check email or request a new code.")
                     .success(false)
                     .build();
         }
@@ -42,19 +61,144 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .email(email)
                 .password(passwordEncoder.encode(request.getPassword()))
                 .roles(new HashSet<>(Set.of(userRole)))
+                .isVerified(false)
                 .build();
 
         User savedUser = userRepository.save(newUser);
-        List<String> roleNames = savedUser.getRoles()
-                .stream()
-                .map(Role::getName)
-                .collect(Collectors.toList());
+        String code = generateVerificationCode();
+        saveAndSendCode(savedUser, code);
 
         return AuthResponseDto.builder()
-                .email(savedUser.getEmail())
-                .roles(roleNames)
-                .message("Account created successfully!")
+                .isVerified(false)
                 .success(true)
+                .message("Registration successful. Verification code sent to email.")
                 .build();
     }
+
+    public AuthResponseDto verify(VerifyRequestDto verifyRequestDto) {
+        String email = verifyRequestDto.getEmail().trim();
+        AuthResponseDto status = checkUserStatus(email);
+        if (status != null) return status;
+
+        User user = userRepository.findByEmail(email).get();
+        EmailVerificationCodes verificationCodes = verificationCodesRepository.findByUser(user)
+                .orElse(null);
+
+        if (verificationCodes == null) {
+            return AuthResponseDto.builder()
+                    .isVerified(false)
+                    .success(false)
+                    .message("Verification code not found. Please request a new one.")
+                    .build();
+        }
+
+        if (verificationCodes.getExpiresAt().isBefore(Instant.now())) {
+            return AuthResponseDto.builder()
+                    .isVerified(false)
+                    .success(false)
+                    .message("Verification code has expired. Please request a new one.")
+                    .build();
+        }
+
+        if (!passwordEncoder.matches(verifyRequestDto.getVerificationCode(), verificationCodes.getCodeHash())) {
+            return AuthResponseDto.builder()
+                    .isVerified(false)
+                    .success(false)
+                    .message("Invalid verification code. Please try again.")
+                    .build();
+        }
+
+        user.setIsVerified(true);
+        userRepository.save(user);
+
+        return AuthResponseDto.builder()
+                .isVerified(true)
+                .success(true)
+                .message("Account verified successfully.")
+                .build();
+    }
+
+    public AuthResponseDto resendCode(EmailRequestDto request) {
+        String email = request.getEmail().trim();
+
+        AuthResponseDto status = checkUserStatus(email);
+        if(status != null) return status;
+
+        User user = userRepository.findByEmail(email).get();
+
+        Optional<EmailVerificationCodes> activeCode = verificationCodesRepository
+                .findByUserAndExpiresAtAfter(user, Instant.now());
+
+        if(activeCode.isPresent()) {
+            return AuthResponseDto.builder()
+                    .isVerified(false)
+                    .success(false)
+                    .message("A verification code is already active. Please check your email.")
+                    .build();
+        }
+
+        String code = generateVerificationCode();
+
+        saveAndSendCode(user, code);
+
+        return AuthResponseDto.builder()
+                .isVerified(false)
+                .success(true)
+                .message("Verification code sent to email.")
+                .build();
+    }
+
+    private AuthResponseDto checkUserStatus(String email) {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+
+        if(optionalUser.isEmpty()) {
+            return AuthResponseDto.builder()
+                    .isVerified(false)
+                    .success(false)
+                    .message("No account found with this email.")
+                    .build();
+        }
+
+        User user = optionalUser.get();
+
+        if(user.getIsVerified()) {
+            return AuthResponseDto.builder()
+                    .isVerified(true)
+                    .success(false)
+                    .message("Account is already verified. Please log in.")
+                    .build();
+        }
+
+        return null;
+    }
+
+    private void saveAndSendCode(User user, String code) {
+        EmailVerificationCodes verificationCodes = verificationCodesRepository.findByUser(user)
+                .orElseGet(() -> {
+                    EmailVerificationCodes codes = new EmailVerificationCodes();
+                    codes.setUser(user);
+                    return codes;
+                });
+
+        verificationCodes.setCodeHash(passwordEncoder.encode(code));
+        verificationCodes.setExpiresAt(Instant.now().plus(15, ChronoUnit.MINUTES));
+        verificationCodesRepository.save(verificationCodes);
+
+        sendVerificationEmail(user.getEmail(), code);
+    }
+
+    public void sendVerificationEmail(String email, String verificationCode) {
+        String subject = "Account Activation";
+        try {
+            emailService.sendVerificationEmail(email, subject, verificationCode);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateVerificationCode() {
+        int code = new Random().nextInt(10000);
+        return String.format("%04d", code);
+    }
+
 }
