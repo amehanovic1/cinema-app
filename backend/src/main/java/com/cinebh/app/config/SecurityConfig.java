fe2317002a740cb85dcf2cf0e29793169d8d853c
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-11 09:10:06 +0100

    backend: Add JWT filter and entry point

--- backend/src/main/java/com/cinebh/app/config/SecurityConfig.java
+++ backend/src/main/java/com/cinebh/app/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cinebh.app.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @EnableMethodSecurity
 @Configuration
