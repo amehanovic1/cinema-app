1d141ee9896b57523b06b4449d77b714d8c8cfdf
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-14 02:17:11 +0100

    backend: Optimize refresh token verification

--- backend/src/main/java/com/cinebh/app/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/cinebh/app/repository/RefreshTokenRepository.java
@@ -1,7 +1,6 @@
 package com.cinebh.app.repository;
 
 import com.cinebh.app.entity.RefreshToken;
-import com.cinebh.app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
