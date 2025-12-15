356ee6bb37038bb53c610aad79f8c2b204b31041
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-15 09:11:32 +0100

    frontend: Refactor email utils

--- frontend/src/utils/emailUtils.js
+++ frontend/src/utils/emailUtils.js
@@ -1,5 +1,5 @@
 export const hideEmail = (email) => {
     const [user, domain] = email.split("@");
-    const censoredUser = user[0] + "*".repeat(user.length - 2) + user.slice(-1);
-    return `${censoredUser}@${domain}`;
+    const hideUser = user[0] + "*".repeat(user.length - 2) + user.slice(-1);
+    return `${hideUser}@${domain}`;
 };
