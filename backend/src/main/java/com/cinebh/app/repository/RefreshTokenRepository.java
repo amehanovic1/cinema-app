b50e5242d433248f2eace7c4279b962a43cadc88
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-16 10:42:26 +0100

    backend: Update import

--- backend/src/main/java/com/cinebh/app/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/cinebh/app/repository/RefreshTokenRepository.java
@@ -1,6 +1,7 @@
 package com.cinebh.app.repository;
 
 import com.cinebh.app.entity.RefreshToken;
+import com.cinebh.app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
