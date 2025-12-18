3ac9d7db387003259bfaf5749c715bb1f544abf8
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-18 09:21:50 +0100

    frontend: Update AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,17 +1,44 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
+import { getCurrentUser, loginUser, logoutUser } from "../services/authService";
+import { useEffect } from "react";
 
-const AuthContext = createContext(null);
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [email, setEmail] = useState("");
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const data = await getCurrentUser();
+                setUser(data);
+            } catch (err) {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchUser();
+    }, []);
+
+    const login = async ({ email, password }) => {
+        await loginUser({ email, password });
+        const data = await getCurrentUser();
+        setUser(data);
+    };
+
+    const logout = async () => {
+        await logoutUser();
+        setUser(null);
+    };
 
     return (
-        <AuthContext.Provider value={{ email, setEmail }}>
+        <AuthContext.Provider value={{ email, setEmail, user, login, logout, loading }}>
             {children}
         </AuthContext.Provider>
     );
 };
 
-export const useAuth = () => {
-    return useContext(AuthContext);
-};
+export default AuthContext;
