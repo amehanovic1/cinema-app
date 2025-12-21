643c6bdf372c28d8c13cb344d76247cc90e0028f
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-21 17:39:20 +0100

    frontend: Remove DrawerContext

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { DrawerProvider } from './context/DrawerContext';
 import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <AuthProvider>
-      <DrawerProvider>
         <App />
-      </DrawerProvider>
     </AuthProvider>
   </BrowserRouter>
 );
