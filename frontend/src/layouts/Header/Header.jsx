625e7e369460f5f0577f44a75c2f488921cfd00c
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-22 09:47:16 +0100

    frontend: Refactor user menu

--- frontend/src/layouts/Header/Header.jsx
+++ frontend/src/layouts/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../../components/Logo/Logo";
 import { ROUTES } from "../../routes/routes";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "../../context/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
@@ -57,9 +57,10 @@ const Header = () => {
                     (
                         <div className="relative">
                             <button
-                                onClick={() => setUserMenuOpen(!userMenuOpen)}
+                                onClick={() => setUserMenuOpen(prev => !prev)}
                                 className="flex items-center gap-1 text-neutral-0 text-semibold shadow-text text-xs sm:text-sm md:text-base 
                                         px-2 py-1 border rounded-lg rounded-neutral-0"
+                                disabled={isDrawerOpen}
                             >
                                 {user.firstName && user.lastName
                                     ? `${user.firstName} ${user.lastName}`
@@ -72,19 +73,20 @@ const Header = () => {
                                 />
                             </button>
 
-                            {userMenuOpen && (
-                                <div className="absolute right-0 mt-1 w-24 bg-neutral-200 rounded-md shadow-lg z-40">
-                                    {userMenuItems.map((item, index) => (
-                                        <button
-                                            key={index}
-                                            onClick={item.onClick}
-                                            className="w-full px-1 py-1 text-right text-neutral-700 text-sm md:text-base
-                                                        hover:bg-neutral-400 hover:text-neutral-0 hover:rounded-md">
-                                            {item.label}
-                                        </button>
-                                    ))}
-                                </div>
-                            )}
+
+                            <ul className={`absolute left-0 right-0 z-40 mt-0 w-full bg-neutral-0 mt-1 
+                                            overflow-y-auto transition-all duration-300 rounded  
+                                             ${userMenuOpen ? "max-h-60 opacity-100" : "max-h-0"}`}>
+                                {userMenuItems?.map((item, index) => (
+                                    <li
+                                        key={index}
+                                        onClick={item.onClick}
+                                        className="py-2 px-3 text-neutral-700 bg-neutral-200 text-xs md:text-sm lg:text-base 
+                                                    hover:bg-neutral-600 hover:text-white">
+                                        {item.label}
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
 
                     ) : (
@@ -99,7 +101,7 @@ const Header = () => {
                 }
             </div>
 
-            <AuthDrawer isOpen={isDrawerOpen} onClose={() => setDrawerOpen(false)} />
+            <AuthDrawer isOpen={isDrawerOpen} onClose={() =>setDrawerOpen(false)} />
 
         </header>
     );
