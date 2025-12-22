646d725c1632ea06ed93ba22502aa87b3fc7aa0c
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-22 08:18:34 +0100

    frontend: Add AuthDrawer

---
+++ frontend/src/pages/AuthDrawer/AuthDrawer.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import Drawer from "../../components/Drawer/Drawer";
+import SignInForm from "../SignInForm/SignInForm";
+import SignUpForm from "../SignUpForm/SignUpForm";
+import SuccessView from "../SuccessView/SuccessView";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
+import { faVideoCamera } from "@fortawesome/free-solid-svg-icons";
+import VerificationCodeForm from "../VerificationCodeForm/VerificationCodeForm";
+
+const AuthDrawer = ({ isOpen, onClose }) => {
+    const viewsMap = {
+        signIn: { component: SignInForm, title: "Welcome back" },
+        signUp: { component: SignUpForm, title: "Hello" },
+        verify: { component: VerificationCodeForm, title: "Activate Account" },
+        signInSuccess: {
+            component: SuccessView, title: "Sign In Successful!",
+            props: {
+                paragraph: "Please, wait. You will be directed to the homepage.",
+                icon: faVideoCamera,
+                autoClose: true
+            }
+        },
+        signUpSuccess: {
+            component: SuccessView, title: "You're all set!",
+            props: {
+                paragraph: "Start exploring latest movies, venues, and ticket options!",
+                icon: faFilm,
+                navigateTo: "/currently-showing",
+                navigateToText: "See movies"
+            }
+        },
+    }
+
+    const [view, setView] = useState("signIn")
+    const [email, setEmail] = useState("")
+
+    useEffect(() => {
+        if (onClose) {
+            setView("signIn");
+            setEmail("")
+        }
+    }, [isOpen])
+
+    const Component = viewsMap[view].component;
+
+    return (
+        <Drawer title={viewsMap[view].title} isOpen={isOpen} onClose={onClose}>
+            {Component && <Component
+                setView={setView}
+                email={email}
+                setEmail={setEmail}
+                onClose={onClose}
+                {...viewsMap[view].props}
+            />}
+        </Drawer>
+    );
+}
+
+export default AuthDrawer;
--- frontend/src/utils/validatorUtils.js
+++ frontend/src/utils/validatorUtils.js
@@ -1,33 +1,36 @@
 import { RE_EMAIL, RE_PASSWORD } from "./regexPatterns";
 
 export const validateEmail = (email) => {
-    if (!email) 
+    if (!email)
         return "Email is required.";
 
-    if (!RE_EMAIL.test(email)) 
+    if (!RE_EMAIL.test(email))
         return "Invalid email format.";
 
     return "";
 }
 
-export const validatePassword = (password) => {
-    if (!password) 
+export const validatePassword = (password, { requiredOnly = false } = {}) => {
+    if (!password)
         return "Password is required.";
 
-    else if (password.length < 8 || password.length > 48)
+    if (requiredOnly)
+        return "";
+
+    if (password.length < 8 || password.length > 48)
         return "Password must be between 8 and 48 characters.";
 
-    if (!RE_PASSWORD.test(password)) 
-       return "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character.";
+    if (!RE_PASSWORD.test(password))
+        return "Password must contain: A-Z, a-z, 0-9, symbol";
 
     return "";
 }
 
 export const validateConfirmPassword = (password, confirm) => {
-    if (!confirm) 
+    if (!confirm)
         return "Confirm password is required.";
 
-    if (password !== confirm) 
+    if (password !== confirm)
         return "Passwords do not match.";
 
     return "";
