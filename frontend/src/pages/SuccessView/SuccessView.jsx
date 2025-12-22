7a7ff1245ccc1e98b2c796140a5e18957a49c3a6
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-22 08:36:37 +0100

    frontend: Refactor SuccessView

--- frontend/src/pages/SuccessView/SuccessView.jsx
+++ frontend/src/pages/SuccessView/SuccessView.jsx
@@ -2,30 +2,31 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import SuccessIcon from "./SuccessIcon";
 
-const SuccessView = ({ paragraph, icon, onClose, navigateTo = null}) => {
-
+const SuccessView = ({ paragraph, icon, onClose, navigateTo = null, navigateToText, autoClose = false }) => {
     const navigate = useNavigate();
 
     useEffect(() => {
+        if (!autoClose) return;
+
         const timer = setTimeout(() => {
-            if (onClose) onClose();
+            onClose();
             navigate("/");
-        }, 2500);
+        }, 3000);
         return () => clearTimeout(timer);
-    }, [onClose, navigate, navigateTo]);
+    }, [onClose, navigate, navigateTo, autoClose]);
 
     return (
-        <div className="flex flex-col items-center justify-center">
-            {paragraph && <p className="text-base md:text-lg text-neutral-300">{paragraph}</p>}
+        <div className="flex flex-col items-center justify-center gap-16">
+            {paragraph && <p className="font-normal text-center text-xs md:text-sm text-neutral-300 w-1/2">{paragraph}</p>}
 
-            <div className="mt-12">
+            <div>
                 <SuccessIcon icon={icon} />
             </div>
 
             {navigateTo && <button
                 onClick={() => navigate(navigateTo)}
-                className="mt-4 py-2 text-base font-semibold w-full rounded-lg bg-dark-red text-neutral-25">
-                Sign In
+                className="py-2 text-base font-semibold w-full rounded-lg bg-dark-red text-neutral-25">
+                {navigateToText}
             </button>
             }
         </div>
