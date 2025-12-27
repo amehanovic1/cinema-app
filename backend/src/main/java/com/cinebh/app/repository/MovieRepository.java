d164f9a4c863265bffd1585658bcb583476c70ab
amehanovic1 <adna.mehanovic1@gmail.com>
2025-12-27 22:32:36 +0100

    backend: Remove findById method

--- backend/src/main/java/com/cinebh/app/repository/MovieRepository.java
+++ backend/src/main/java/com/cinebh/app/repository/MovieRepository.java
@@ -4,10 +4,7 @@ import com.cinebh.app.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface MovieRepository extends JpaRepository<Movie, UUID>, JpaSpecificationExecutor<Movie> {
-
-    Optional<Movie> findById(UUID id);
 }
