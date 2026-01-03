6f4a18c8b47a3013fe3a32ec6a36a4903de1c8b3
amehanovic1 <adna.mehanovic1@gmail.com>
2026-01-03 19:17:48 +0100

    frontend: Add skeleton for movie details page

--- frontend/src/pages/MovieDetails/MovieDetails.jsx
+++ frontend/src/pages/MovieDetails/MovieDetails.jsx
@@ -326,7 +326,6 @@ const MovieDetails = () => {
                                 }
                             />
                         </div>
-
                     </div>
                 ) : (
                     <NoDataFound
