af7f477ac1d47fe38e27703ab525414718604c7c
amehanovic1 <adna.mehanovic1@gmail.com>
2026-01-06 10:07:17 +0100

    frontend: Add SeatTypeLegend component

--- backend/src/main/java/com/cinebh/app/service/impl/SeatTypeServiceImpl.java
+++ backend/src/main/java/com/cinebh/app/service/impl/SeatTypeServiceImpl.java
@@ -1,12 +1,14 @@
 package com.cinebh.app.service.impl;
 
 import com.cinebh.app.dto.SeatTypeDto;
+import com.cinebh.app.entity.SeatType;
 import com.cinebh.app.mapper.SeatTypeMapper;
 import com.cinebh.app.repository.SeatTypeRepository;
 import com.cinebh.app.service.SeatTypeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,6 +22,7 @@ public class SeatTypeServiceImpl implements SeatTypeService {
     public List<SeatTypeDto> getAllSeatTypes() {
         return seatTypeRepository.findAll()
                 .stream()
+                .sorted(Comparator.comparingDouble(SeatType::getPrice))
                 .map(seatTypeMapper::toDto)
                 .toList();
     }
