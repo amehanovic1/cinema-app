9545133d0c260821cfb79d99afe8091849f13686
amehanovic1 <adna.mehanovic1@gmail.com>
2026-01-21 15:11:25 +0100

    frontend: Update data-testids to be component-specific for ContentSection component

--- frontend/src/components/ContentSection/ContentSection.jsx
+++ frontend/src/components/ContentSection/ContentSection.jsx
@@ -12,7 +12,7 @@ const ContentSection = ({ title, linkTo = null, items, getAll, renderItem }) => 
             <div className="flex items-center justify-between">
                 <h1
                     className="font-bold text-base sm:text-xl md:text-2xl lg:text-3xl text-neutral-800 mb-2 sm:mb-3 md:mb-4 lg:mb-5"
-                    data-testid="section-title"
+                    data-testid="content-section-title"
                 >
                     {title}
                 </h1>
@@ -21,7 +21,7 @@ const ContentSection = ({ title, linkTo = null, items, getAll, renderItem }) => 
                     <NavLink
                         to={linkTo}
                         className="font-semibold text-dark-red text-xs sm:text-sm md:text-base lg:text-lg"
-                        data-testid="section-see-all"
+                        data-testid="content-section-see-all"
                     >
                         See all
                     </NavLink>
@@ -30,7 +30,7 @@ const ContentSection = ({ title, linkTo = null, items, getAll, renderItem }) => 
             </div>
 
 
-            <div className="grid gap-4 justify-center grid-cols-[repeat(auto-fit,minmax(8rem,1fr))]" data-testid="section-grid">  
+            <div className="grid gap-4 justify-center grid-cols-[repeat(auto-fit,minmax(8rem,1fr))]" data-testid="content-section-grid">  
                 {
                 items?.content?.length > 0 &&
                 items.content.map(item =>
@@ -47,7 +47,7 @@ const ContentSection = ({ title, linkTo = null, items, getAll, renderItem }) => 
             </div>
 
             {items?.content?.length > 0 && (
-                <div className="w-full flex justify-center mt-4 sm:mt-6 md:mt-8 lg:mt-10" data-testid="section-pagination">
+                <div className="w-full flex justify-center mt-4 sm:mt-6 md:mt-8 lg:mt-10" data-testid="content-section-pagination">
                     <Pagination
                         number={items.number}
                         totalElements={items.totalElements}
