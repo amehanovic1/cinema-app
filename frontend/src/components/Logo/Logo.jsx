011e9fc664bf2fad715c775d5a0f4bbe0f0aa8d8
amehanovic1 <adna.mehanovic1@gmail.com>
2026-01-21 15:17:09 +0100

    frontend: Update data-testids to be component-specific for Logo  component

--- frontend/src/components/Logo/Logo.jsx
+++ frontend/src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import { faVideoCamera } from "@fortawesome/free-solid-svg-icons"
 const Logo = () => {
     return (
         <div
-            className="flex items-center justify-center bg-neutral-25 rounded-md p-1" data-testid="app-logo">
+            className="flex items-center justify-center bg-neutral-25 rounded-md p-1" data-testid="logo-container">
             <FontAwesomeIcon
                 icon={faVideoCamera}
                 className="text-dark-red text-xs sm:text-sm md:text-base"
