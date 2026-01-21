0e10f6ea6f3957675211f330e5808422fe119652
amehanovic1 <adna.mehanovic1@gmail.com>
2026-01-21 15:07:40 +0100

    frontend: Update data-testids to be component-specific for Carousel component

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -4,6 +4,14 @@ const Carousel = ({ items, getImage, renderItem = [], autoSlide = false, autoSli
 
     const [activeIndex, setActiveIndex] = useState(0)
 
+    const getFormattedId = (title, index) => {
+        return (title || `slide-${index}`)
+            .toLowerCase()
+            .trim()
+            .replace(/\s+/g, '-')
+            .replace(/[^a-z0-9-]/g, "");
+    };
+
     const gotToNextSlide = () => {
         setActiveIndex((prevIndex) => prevIndex === items.length - 1 ? 0 : prevIndex + 1)
     };
@@ -16,7 +24,7 @@ const Carousel = ({ items, getImage, renderItem = [], autoSlide = false, autoSli
     }, [autoSlide, autoSlideInterval, items.length]);
 
     return (
-        <div className='relative w-full h-full overflow-hidden' data-testid="carousel-container">
+        <div className='relative w-full h-full overflow-hidden' data-testid="carousel-main-container">
             <>
                 <div className={`flex  h-full
                     ${activeIndex === 0 ? "" : "transition-transform duration-800 ease-in-out"}`}
@@ -24,33 +32,38 @@ const Carousel = ({ items, getImage, renderItem = [], autoSlide = false, autoSli
                     data-testid="carousel-track"
                 >
 
-                    {items.map((item, index) => (
-                        <div
-                            key={index}
-                            className={`w-full flex-shrink-0 relative h-full`}
-                        >
-                            <img
-                                src={getImage(item)}
-                                alt={item.title}
-                                className="w-full h-full object-cover"
+                    {items.map((item, index) => {
+
+                        const slideId = getFormattedId(item.title, index);
+
+                        return (
+                            <div
+                                key={index}
+                                className="w-full flex-shrink-0 relative h-full"
+                                data-testid={`carousel-slide-${slideId}`}
                             >
-                            </img>
-
-                            {renderItem && renderItem(item)}
-
-                        </div>
-                    ))}
+                                <img
+                                    src={getImage(item)}
+                                    alt={item.title}
+                                    className="w-full h-full object-cover"
+                                    data-testid={`carousel-img-${slideId}`}
+                                />
+                                {renderItem && renderItem(item)}
+                            </div>
+                        );
+                    })}
 
                 </div>
 
                 <div
                     className='absolute bottom-4 sm:bottom-6 w-full flex justify-center gap-1.5 sm:gap-2 md:gap-3 lg:gap-4 z-20'
-                    data-testid="carousel-indicators"
+                    data-testid="carousel-indicators-container"
                 >
                     {items?.map((_, index) =>
                         <button
                             key={index}
                             onClick={() => goToSlide(index)}
+                            data-testid={`carousel-indicator-dot-${index}`}
                             className={`rounded-full transition-colors duration-300
                                 ${index === activeIndex
                                     ? "bg-neutral-50"
