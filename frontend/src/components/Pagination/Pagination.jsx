546a47ee776467bb05ff9150b70d8bc4425a0062
amehanovic1 <adna.mehanovic1@gmail.com>
2026-01-21 15:38:32 +0100

    frontend: Update data-testids to be component-specific for Pagination component

--- frontend/src/components/Pagination/Pagination.jsx
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -8,16 +8,16 @@ const Pagination = ({ number, totalElements, size, hasNext, hasPrevious, onPageC
     return (
         <div
             className="w-full flex flex-col sm:flex-row justify-end items-center gap-1 sm:gap-2 md:gap-4 lg:gap-5"
-            data-testid="pagination-container"
+            data-testid="pagination-main-nav"
         >
             <p className="font-regular text-neutral-800 text-xs sm:text-sm md:text-base lg:text-lg"
-                data-testid="pagination-info"
+                data-testid="pagination-summary-text"
             > Showing
                 <span
-                    className="font-semibold" data-testid="pagination-current-end"> {end}
+                    className="font-semibold" data-testid="pagination-count-current"> {end}
                 </span> out of
                 <span
-                    className="font-semibold" data-testid="pagination-total"> {totalElements}
+                    className="font-semibold" data-testid="pagination-count-total"> {totalElements}
                 </span>
             </p>
 
@@ -25,7 +25,7 @@ const Pagination = ({ number, totalElements, size, hasNext, hasPrevious, onPageC
                 <button
                     onClick={() => onPageChange(number - 1)}
                     disabled={!hasPrevious}
-                    data-testid="pagination-prev"
+                    data-testid="pagination-btn-prev"
                     className="inline-flex items-center justify-center bg-neutral-0 disabled:opacity-50 border border-neutral-200 rounded-lg p-1 sm:p-1.5 md:p-2 lg:p-3 group">
                     <FontAwesomeIcon
                         icon={faArrowLeft}
@@ -36,7 +36,7 @@ const Pagination = ({ number, totalElements, size, hasNext, hasPrevious, onPageC
                 <button
                     onClick={() => onPageChange(number + 1)}
                     disabled={!hasNext}
-                    data-testid="pagination-next"
+                    data-testid="pagination-btn-next"
                     className="inline-flex items-center justify-center bg-neutral-0 disabled:opacity-50 border border-neutral-200 rounded-lg p-1 sm:p-1.5 md:p-2 lg:p-3 group">
                     <FontAwesomeIcon
                         icon={faArrowRight}
