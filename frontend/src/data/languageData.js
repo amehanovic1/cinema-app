c0927ed36162d231cadfac778ae7479480119460
amehanovic1 <adna.mehanovic1@gmail.com>
2026-02-07 22:15:44 +0100

    frontend: Add language and PG rating data

---
+++ frontend/src/data/languageData.js
@@ -0,0 +1,7 @@
+export const language = [
+    { id: "en", name: "English" },
+    { id: "bs", name: "Bosnian" },
+    { id: "sr", name: "Serbian" },
+    { id: "hr", name: "Croatian" },
+    { id: "de", name: "German" }
+]
---
+++ frontend/src/data/pgRatingData.js
@@ -0,0 +1,7 @@
+export const pgRating = [
+    { id: "g", name: "G" },
+    { id: "pg", name: "PG" },
+    { id: "pg13", name: "PG-13" },
+    { id: "r", name: "R" },
+    { id: "nc17", name: "NC-17" }
+]
